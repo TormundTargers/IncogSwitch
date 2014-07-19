24c7d042a7ac00252d5003d47c62a1387dc10375
Jamie Redding <jamieredding2694@gmail.com>
2014-07-19 17:21:47 +0100

    Blacklist some chrome:// urls, fixed bug when rapidly running extension, changed version number

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
 var currentUrl;
 var newWindowMode;
 var windowToAddTo = -1;
+var validIncog = true;
+var invalidURL = ["chrome://bookmarks/#1",
+					"chrome://chrome/",
+					"chrome://devices/",
+					"chrome://extensions/",
+					"chrome://help/",
+					"chrome://history/",
+					"chrome://settings/",
+					"chrome://suggestions/",
+					"chrome://thumbnails/"];
 
 chrome.browserAction.onClicked.addListener(function(tab) {
 	// Get the mode (normal or incognito) to change the tab to
@@ -15,22 +25,23 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 				windowToAddTo = allWindows[i].id;
 			}
 		}
-	});
-
-	// Perform the mode switch
-	chrome.tabs.query({currentWindow: true, active: true}, function(tabList) {
-		// Get the url of the current tab and encode it
-		currentUrl = encodeURI(tabList[0].url);
-		// Check if tab is a settings tab
-		if(currentUrl.indexOf("chrome://") == -1) {
-			if(windowToAddTo == -1) {
-				chrome.windows.create({url: currentUrl, incognito: newWindowMode});
+		// Perform the mode switch
+		chrome.tabs.query({currentWindow: true, active: true}, function(tabList) {
+			// Get the url of the current tab and encode it
+			currentUrl = encodeURI(tabList[0].url);
+			// Check if tab is valid in both incog and normal modes
+			if(invalidURL.indexOf(currentUrl) == -1) {
+				if(windowToAddTo == -1) {
+					chrome.windows.create({url: currentUrl, incognito: newWindowMode});
+				}
+				else {
+					chrome.tabs.create({windowId: windowToAddTo, url: currentUrl, active: true});
+					chrome.windows.update(windowToAddTo, {focused: true});
+				}
+				chrome.tabs.remove(tabList[0].id);
 			}
-			else {
-				chrome.tabs.create({windowId: windowToAddTo, url: currentUrl, active: true});
-				chrome.windows.update(windowToAddTo, {focused: true});
-			}
-			chrome.tabs.remove(tabList[0].id);
-		}
+		});
 	});
+	// Reset windowToAddTo in case chrome doesn't clean up
+	windowToAddTo = -1;
 });
