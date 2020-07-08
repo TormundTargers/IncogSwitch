23a5aa91eccc34e10c45344b1aef09e729e5a711
Jamie Redding <jamieredding2694@gmail.com>
2020-07-08 18:32:41 +0100

    Adding support for moving highlighted tabs.

--- background.js
+++ background.js
@@ -1,47 +1,73 @@
-var currentUrl;
-var newWindowMode;
-var windowToAddTo = -1;
-var invalidURL = ["chrome://bookmarks/#1",
-					"chrome://chrome/",
-					"chrome://devices/",
-					"chrome://extensions/",
-					"chrome://help/",
-					"chrome://history/",
-					"chrome://settings/",
-					"chrome://suggestions/",
-					"chrome://thumbnails/"];
+var invalidURL = [
+    "chrome://bookmarks/#1",
+    "chrome://chrome/",
+    "chrome://devices/",
+    "chrome://extensions/",
+    "chrome://help/",
+    "chrome://history/",
+    "chrome://settings/",
+    "chrome://suggestions/",
+    "chrome://thumbnails/",
+];
 
-chrome.browserAction.onClicked.addListener(function() {
-	// Swap the window mode (normal or incognito)
-	chrome.windows.getCurrent(function(window_) {
-		newWindowMode = window_.incognito !== true;
-	});
+chrome.browserAction.onClicked.addListener(function () {
+    // Swap the window mode (normal or incognito)
+    chrome.windows.getCurrent(function (window_) {
+        let newWindowMode = !window_.incognito;
 
-	// Look for a existing window to add the tab to
-	chrome.windows.getAll(function(allWindows) {
-		for(var i = 0; i < allWindows.length; i++) {
-			if(allWindows[i].incognito === newWindowMode) {
-				windowToAddTo = allWindows[i].id;
-			}
-		}
+        chrome.windows.getAll(function (allWindows) {
+            let windowId = getWindowIdToAddTo(allWindows, newWindowMode);
+            chrome.tabs.query(
+                { currentWindow: true, highlighted: true },
+                function (tabList_) {
+                    const tabList = tabList_.filter(
+                        (tab) => invalidURL.indexOf(tab.url) === -1
+                    );
+                    const [first, ...rest] = tabList;
 
-		// Perform the mode switch
-		chrome.tabs.query({currentWindow: true, active: true}, function(tabList) {
-			// Get the url of the current tab
-			currentUrl = tabList[0].url;
-			// Check if tab is valid in both incog and normal modes
-			if(invalidURL.indexOf(currentUrl) === -1) {
-				if(windowToAddTo === -1) {
-					chrome.windows.create({url: currentUrl, incognito: newWindowMode});
-				}
-				else {
-					chrome.tabs.create({windowId: windowToAddTo, url: currentUrl, active: true});
-					chrome.windows.update(windowToAddTo, {focused: true});
-				}
-				chrome.tabs.remove(tabList[0].id);
-			}
-		});
-	});
-	// Reset windowToAddTo in case chrome doesn't clean up
-	windowToAddTo = -1;
+                    if (windowId === -1) {
+                        chrome.windows.create(
+                            { url: first.url, incognito: newWindowMode },
+                            (newWindow) =>
+                                removeTabAndMoveRemaining(
+                                    newWindow.id,
+                                    first.id,
+                                    rest
+                                )
+                        );
+                    } else {
+                        moveTabs(windowId, tabList);
+                    }
+                }
+            );
+        });
+    });
 });
+
+function moveTabs(windowId, tabs) {
+    if (tabs.length == 0) {
+        chrome.windows.update(windowId, {
+            focused: true,
+        });
+        return;
+    }
+
+    const [tabToMove, ...remainingTabs] = tabs;
+    chrome.tabs.create(
+        { windowId: windowId, url: tabToMove.url, active: true },
+        (_) => removeTabAndMoveRemaining(windowId, tabToMove.id, remainingTabs)
+    );
+}
+
+function removeTabAndMoveRemaining(windowId, tabId, remainingTabs) {
+    chrome.tabs.remove(tabId, (_) => moveTabs(windowId, remainingTabs));
+}
+
+function getWindowIdToAddTo(windows, windowMode) {
+    for (const window of windows) {
+        if (window.incognito === windowMode) {
+            return window.id;
+        }
+    }
+    return -1;
+}
